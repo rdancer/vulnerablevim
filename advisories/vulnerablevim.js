12f7037019d288900b9581b948e69489b013701d
Jan Minar <rdancer@rdancer.org>
2015-12-03 06:23:51 -0500

    Add highlight.js syntax highlighting
    Without explicit highlighting directives, heuristic is used to highlight code
    blocks. This does not necessarily lead to good highlighting.

---
+++ advisories/vulnerablevim.js
@@ -0,0 +1,14 @@
+"use strict";
+
+(function loadHighlightJs()
+{
+  var script = document.createElement("script");
+
+  script.src = "../highlight/highlight.pack.js";
+  script.type = "text/javascript";
+
+  document.head.appendChild(script);
+
+  console.log("Highlight script loaded.");
+})();
+
